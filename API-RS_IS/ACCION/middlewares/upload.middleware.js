1edc31b1173d303b151ff663cd17697e437417b3
JESUS DIAZ <diazmontejo125@gmail.com>
2024-05-31 19:42:34 -0600

    Finalizado1

--- API-RS_IS/ACCION/middlewares/upload.middleware.js
+++ API-RS_IS/ACCION/middlewares/upload.middleware.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null,  + '-' + file.originalname);
   }
 });
 
